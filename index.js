ba7177d11ed72460e9d913e2f05f6b59494f08c5
amine <jmalmohamedamine1@gmail.com>
2024-10-16 23:22:58 +0100

    last commit

--- index.js
+++ index.js
@@ -6,19 +6,9 @@ var express = require("express");
 var app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const dns = require("dns");
+const { URL } = require("url");
 
-const urlSchema = new mongoose.Schema({
-  originalUrl: {
-    type: String,
-    required: true,
-  },
-  shortUrl: {
-    type: String,
-    required: true,
-  },
-});
-
-const Url = mongoose.model("Url", urlSchema);
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -32,15 +22,60 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 //connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Successfully connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
 // your first API endpoint...
-app.get("/api/shorturl", function (req, res) {
-  console.log(req.body);
+app.post("/api/shorturl", function (req, res) {
+  const inputUrl = req.body.url;
+
+  try {
+    const urlObj = new URL(inputUrl); // Correct URL parsing
+
+    dns.lookup(urlObj.hostname, async (err, address) => {
+      if (err || !address) {
+        res.json({ error: "invalid URL" });
+      } else {
+        const urlcount = await Url.countDocuments({});
+
+        const result = await Url.create({
+          originalUrl: inputUrl,
+          shortUrl: urlcount,
+        });
+
+        console.log(result);
+        res.json({
+          original_url: inputUrl,
+          short_url: urlcount,
+        });
+      }
+    });
+  } catch (err) {
+    res.json({ error: "invalid URL" });
+  }
+});
+
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlDoc = await Url.findOne({ shortUrl: shorturl });
+  res.redirect(urlDoc.originalUrl);
 });
 
 // Listen on port set in environment variable or default to 3000
